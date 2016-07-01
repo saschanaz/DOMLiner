f5093c0adbeeed7e51c12fc6fbfdd2e598d6d38c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-07-01 22:51:49 +0900

    add lib dir

---
+++ lib/domliner.d.ts
@@ -0,0 +1,14 @@
+interface DOMDecorations {
+    [key: string]: any;
+}
+declare class DOMLiner {
+    document: Document;
+    constructor(document: Document);
+    element<T extends Element>(tagName: string, decorations?: DOMDecorations, children?: (string | Node)[]): T;
+    element<T extends Element>(tagName: string, decorations?: DOMDecorations, textContent?: string): T;
+    private _propertyAssign(element, propertyAnnotation, propertyValue);
+    private static _globalLiner;
+    static element<T extends Element>(tagName: string, decorations?: DOMDecorations, children?: (string | Node)[]): T;
+    static element<T extends Element>(tagName: string, decorations?: DOMDecorations, textContent?: string): T;
+    static access<T extends Element>(element: T, fn: (element: T) => any): T;
+}
