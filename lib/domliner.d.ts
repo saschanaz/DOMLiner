a978d0072770df71f791375b5a12668ea238d29a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-05-08 18:43:31 +0900

    lib update

--- lib/domliner.d.ts
+++ lib/domliner.d.ts
@@ -7,9 +7,13 @@ declare class DOMLiner {
     constructor(document: Document);
     element<T extends keyof ElementTagNameMap>(tag: T | ElementTagNameMap[T], decorations?: DOMDecorations<ElementTagNameMap[T]>, children?: (string | Node)[]): ElementTagNameMap[T];
     element<T extends keyof ElementTagNameMap>(tag: T | ElementTagNameMap[T], decorations?: DOMDecorations<ElementTagNameMap[T]>, textContent?: string): ElementTagNameMap[T];
+    element<T extends Element>(tag: T, decorations?: DOMDecorations<T>, children?: (string | Node)[]): T;
+    element<T extends Element>(tag: T, decorations?: DOMDecorations<T>, textContent?: string): T;
     private _propertyAssign(element, propertyAnnotation, propertyValue);
     private static _globalLiner;
     static element<T extends keyof ElementTagNameMap>(tagName: T | ElementTagNameMap[T], decorations?: DOMDecorations<ElementTagNameMap[T]>, children?: (string | Node)[]): ElementTagNameMap[T];
     static element<T extends keyof ElementTagNameMap>(tagName: T | ElementTagNameMap[T], decorations?: DOMDecorations<ElementTagNameMap[T]>, textContent?: string): ElementTagNameMap[T];
+    static element<T extends Element>(tag: T, decorations?: DOMDecorations<T>, children?: (string | Node)[]): T;
+    static element<T extends Element>(tag: T, decorations?: DOMDecorations<T>, textContent?: string): T;
     static access<T extends Element>(element: T, fn: (element: T) => any): T;
 }
